b25bf06b58713e901f8a218d22c20ae0fdfe49c5
zbelgaoui <zineb.belgaoui@gmail.com>
2025-11-13 10:32:22 -0600

    Update index.php

--- ch11_ex1/index.php
+++ ch11_ex1/index.php
@@ -6,9 +6,9 @@ if ($task_list === NULL) {
     $task_list = [];
     
 //    add some hard-coded starting values to make testing easier
-//    $task_list[] = 'Write chapter';
-//    $task_list[] = 'Edit chapter';
-//    $task_list[] = 'Proofread chapter';
+$task_list[] = 'Write chapter';
+$task_list[] = 'Edit chapter';
+$task_list[] = 'Proofread chapter';
 }
 
 //get action variable from POST
